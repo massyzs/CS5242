0db17f6919da33912e6fce9915f152e01739289d
Yao TONG <tongyao9@yeah.net>
2023-04-02 21:37:01 +0800

    parallel

--- run_parallel.sh
+++ run_parallel.sh
@@ -6,7 +6,8 @@ dropout_opts=(0 1)
 weight_decay_opts=(0 1)
 
 run_command() {
-    cuda=$1
+    gpu_idx=$1
+    cuda="cuda:$gpu_idx"
     norm=$2
     epoch=$3
     dropout=$4
@@ -15,7 +16,7 @@ run_command() {
     activ=$7
     logfile=$8
 
-    CUDA_VISIBLE_DEVICES="$cuda" nohup python -u main.py --norm_type "$norm" --epoch "$epoch" --dropout "$dropout" --weight_decay "$weight_decay" --opt "$opt" --activation "$activ" &> "$logfile"
+    CUDA_VISIBLE_DEVICES="$gpu_idx" python -u main.py --norm_type "$norm" --epoch "$epoch" --dropout "$dropout" --weight_decay "$weight_decay" --opt "$opt" --activation "$activ" &> "$logfile"
 }
 
 export -f run_command
@@ -23,12 +24,17 @@ export -f run_command
 for activ in "${activ_opts[@]}"; do
     for opt in "${opt_opts[@]}"; do
         for norm in "${norm_opts[@]}"; do
-            run_command 0 "$norm" 40 0 0 "$opt" "$activ" "$norm-$opt-$activ-Nodropout-NoL2.log" &
-            run_command 0 "$norm" 40 1 0 "$opt" "$activ" "$norm-$opt-$activ-Dropout-NoL2.log" &
-            run_command 1 "$norm" 40 0 1 "$opt" "$activ" "$norm-$opt-$activ-Nodropout-L2.log" &
-            run_command 1 "$norm" 40 1 1 "$opt" "$activ" "$norm-$opt-$activ-Dropout-L2.log" &
+            for dropout in "${dropout_opts[@]}"; do
+                for weight_decay in "${weight_decay_opts[@]}"; do
+                    run_command 0 "$norm" 40 0 0 "$opt" "$activ" "$norm-$opt-$activ-Nodropout-NoL2.log" &
+                    run_command 1 "$norm" 40 1 0 "$opt" "$activ" "$norm-$opt-$activ-Dropout-NoL2.log" &
+                    run_command 2 "$norm" 40 0 1 "$opt" "$activ" "$norm-$opt-$activ-Nodropout-L2.log" &
+                    run_command 3 "$norm" 40 1 1 "$opt" "$activ" "$norm-$opt-$activ-Dropout-L2.log" &
+                done
+            done
         done
+        wait # wait for all commands for this optimization option to finish
     done
 done
 
-wait
+wait # wait for all commands to finish
